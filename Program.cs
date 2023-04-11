460177b71540e60d6b7c6e4806ba9fbf49f391c8
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-04-11 18:17:57 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices.ObjectiveC;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+
+            //  PurchaseList.Orders();
+            // Swap_Position.swapping();
+            //DuplicateLetters.Counts("aaaajay");
+            //Chapters.FindChapters();
+            Quiz.FindtheNumber();
+
+            //----------------TO FETCH THE REFLECTION DATA-------------------
+            Console.WriteLine();
+            Type types = typeof(Quiz);
+            Console.WriteLine("------------Constructors----------");
+            ConstructorInfo[] constructors = types.GetConstructors();
+            Console.WriteLine("Constructor Length : "+constructors.Length);
+            foreach (ConstructorInfo data in constructors) { Console.WriteLine(data.Name); }
+            Console.WriteLine("------------Methods----------------");
+            MethodInfo[] methods = types.GetMethods();
+            Console.WriteLine("Methods Length : " + methods.Length);
+            foreach (MethodInfo data in methods) { Console.WriteLine(data.Name); }
+            Console.WriteLine("-------------Properties-----------------");
+            PropertyInfo[] properties = types.GetProperties();
+            Console.WriteLine("Properties Length : " + properties.Length);
+            foreach (PropertyInfo data in properties) { Console.WriteLine(data.Name); }
+
+            //---------------create an object using default constructor
+
+            object newobject = Activator.CreateInstance(types);
+
+            types.GetMethod("FindtheNumber").Invoke(newobject, null);
+
+            ConstructorInfo getdata = types.GetConstructor(new Type[] { typeof(int) });
+            object[] parameterr = {45};
+            object invoking = getdata.Invoke(parameterr);
+
+            Quiz Class = (Quiz)invoking;
+
+           // Class.FindEvenDigitNumber();
+
+
+        }
+    }
+}
---
+++ Quiz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Quiz
+    {
+        public static void FindtheNumber()
+        {
+            Console.WriteLine("Enter the Number");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int closenumber = N;
+
+            if(N<0)
+            {
+                closenumber = -closenumber;
+            }
+            while(true)
+            {
+                closenumber++;
+
+                if(FindEvenDigitNumber(closenumber))
+                {
+                    break;
+                }
+            }
+
+            if(N<0)
+            {
+                closenumber=-closenumber;
+            }
+            Console.WriteLine("The Closest Even Digit Number of the given input is "+closenumber);
+        }
+
+        private static bool FindEvenDigitNumber(int number)
+        {
+            foreach(char digit in number.ToString())
+            {
+                int Value = (int)char.GetNumericValue(digit);
+                if(Value%2!=0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
